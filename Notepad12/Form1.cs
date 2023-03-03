4ce058661e1929484eb8e2e4f8eb04c2077b2e4f
Makarova Darya <makarovadarya2704@gmail.com>
2023-03-03 19:40:06 +0300

    Add project files.

---
+++ Notepad12/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Printing;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Notepad12
+{
+    public partial class Form1 : Form
+    {
+        private string _openFile;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void шрифтToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog myFont = new FontDialog();
+            if (myFont.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Font = myFont.Font;
+            }
+        }
+
+        private void времяИДатаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += DateTime.Now;
+        }
+
+        private void новоеОкноToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog Sdialog = new SaveFileDialog();
+            Sdialog.Filter = "all (*.*) |";
+            if (Sdialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(Sdialog.FileName, richTextBox1.Text);
+                _openFile = Sdialog.FileName;
+            }
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Fdialog = new OpenFileDialog();
+            Fdialog.Filter = "all (*.*) |";
+            if (Fdialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(Fdialog.FileName);
+                _openFile = Fdialog.FileName;
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.WriteAllText(_openFile, richTextBox1.Text);
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Не удалось сохранить файл");
+            }
+        }
+
+        private void печатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PrintDocument pDoc = new PrintDocument();
+            pDoc.PrintPage += PrintPageD;
+            PrintDialog pDialog = new PrintDialog();
+            pDialog.Document = pDoc;
+            if (pDialog.ShowDialog() == DialogResult.OK)
+            {
+                pDialog.Document.Print();
+            }
+        }
+        public void PrintPageD(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(richTextBox1.Text, richTextBox1.Font, Brushes.Black, 0, 0);
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        private void скопироватьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(richTextBox1.SelectedText);
+        }
+        private void вырезатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(richTextBox1.SelectedText);
+            richTextBox1.Text = richTextBox1.Text.Remove(richTextBox1.SelectionStart,richTextBox1.SelectionLength);
+        }
+        private void вставитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = richTextBox1.Text.Substring(0, richTextBox1.SelectionStart) + Clipboard.GetText() + richTextBox1.Text.Substring(richTextBox1.SelectionStart, richTextBox1.Text.Length - richTextBox1.SelectionStart);
+        }
+    }
+}
